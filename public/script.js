d38013ce3f01479c0ab14d389104099640160f69
lauraen03 <lauraen@email.sc.edu>
2023-11-05 18:19:28 -0500

    reading server side json

---
+++ public/script.js
@@ -0,0 +1,54 @@
+const showRecipes = async () => {
+  try {
+    const recipesJSON = await getRecipes();
+    const appElement = document.getElementById('app');
+
+    if (!recipesJSON) {
+      console.log('Invalid load of JSON');
+      return;
+    }
+
+    recipesJSON.forEach((recipe) => {
+      const itemDiv = document.createElement('div');
+      itemDiv.className = 'item';
+      appElement.appendChild(itemDiv);
+
+      const h2 = document.createElement('h2');
+      h2.innerHTML = recipe.name;
+      itemDiv.appendChild(h2);
+
+      const ul = document.createElement('ul');
+      recipe.ingredients.forEach((ingredient) => {
+        const li = document.createElement('li');
+        li.innerHTML = ingredient;
+        ul.appendChild(li);
+      });
+      itemDiv.appendChild(ul);
+
+      const p = document.createElement('p');
+      p.innerHTML = recipe.instructions;
+      itemDiv.appendChild(p);
+
+      const img = document.createElement('img');
+      img.src = recipe.image;
+      img.alt = `${recipe.name} Image`;
+      img.style.width = '200px'; // Set the fixed width
+      img.style.height = '200px'; // Set the fixed height
+      itemDiv.appendChild(img);
+    });
+  } catch (error) {
+    console.log('Error retrieving JSON');
+  }
+};
+
+const getRecipes = async () => {
+  try {
+    const response = await fetch('/api/data');
+    return response.json();
+  } catch (error) {
+    console.log('Error retrieving JSON');
+    return null;
+  }
+};
+
+window.onload = () => showRecipes();
---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+const port = 1800; // Change the port to 1800
+
+// Serve static files (e.g., HTML, CSS, and images) from a directory (public in this case)
+app.use(express.static('public'));
+
+// Your array of recipes
+const recipes = [
+  {
+    id: 1,
+    name: "Chocolate Chip Cookies",
+    ingredients: [
+      "2 1/4 cups all-purpose flour",
+      "1/2 teaspoon baking soda",
+      "1 cup unsalted butter, at room temperature",
+      "1/2 cup granulated sugar",
+      "1 cup brown sugar",
+      "2 large eggs",
+      "2 teaspoons pure vanilla extract",
+      "2 cups semisweet and/or milk chocolate chips",
+
+      // ... Add the ingredients for Chocolate Chip Cookies
+    ],
+    instructions: "Preheat the oven, mix dry ingredients, cream butter and sugars, add eggs and vanilla, mix in chocolate chips, drop spoonfuls of dough onto baking sheets, and bake.",
+    image: "chocolate-chip-cookies.jpg",
+  },
+  {
+    id: 2,
+    name: "Yellow Cake",
+    ingredients: [
+      "2 1/2 cups all-purpose flour",
+      "2 1/2 teaspoons baking powder",
+      "1/2 teaspoon salt",
+      "1 cup unsalted butter, at room temperature",
+      "2 cups granulated sugar",
+      "4 large eggs",
+      "1 tablespoon pure vanilla extract",
+      "1 cup whole milk",
+    ],
+    instructions: "Preheat the oven, mix dry ingredients, cream butter and sugar, add eggs and vanilla, add dry and wet ingredients alternately, and bake.",
+    image: "yellow-cakes.jpg",
+  }
+  // Add more recipes here
+];
+
+// Create a route to serve the JSON data
+app.get('/api/data', (req, res) => {
+  res.json(recipes);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
